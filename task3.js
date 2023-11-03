9e2eae0fd0d8785939adae2b941a7f877b01c336
xel26 <zhxo111nvshq@gmail.com>
2023-11-03 12:47:18 +0700

    revisi task3 & add program algoritma task4

--- task3.js
+++ task3.js
@@ -1,43 +1,57 @@
-const potonganHarga = (potongan, harga, persentase) => {                                // membuat fungsi yg menerima 3 parameter untuk menentukan potongan harga . di buatkan fungsi agar tidak ada kode yg berulang
-    const discon = harga * persentase/100                                               // menghitung discon
-    if(discon < potongan){                                                              // jika discon lebih kecil dari potongan maka REASSIGNMENT potongan dengan nilai discon lalu return nilainya
-        return potongan = discon
+{
+    const fazzFood = (harga, voucher, jarak, pajak) => {                                    // membuat fungsi yang menerima 4 parameter
+        if (jarak <= 0 || harga <= 0){                                                      // metode guarding untuk memeriksa jika inputan jarak atau harga kurang dari atau sama dengan 0 maka program di hentikan dan menampilkan pesan tersebut
+            return console.log("input tidak valid")
+        }
+
+        const vouchers = [
+            {
+                nama: "FAZZFOOD50",
+                minHarga: 50000,
+                maxPotongan: 50000,
+                discon: 50/100
+            },
+            {
+                nama: "DITRAKTIR60",
+                minHarga: 25000,
+                maxPotongan: 30000,
+                discon: 60/100
+            }
+        ]
+
+        const promo = vouchers.map(item => item.nama).indexOf(voucher)                      // mengecek apakah ada voucher yang dipilih ada di dalam array. jika ada akan mengembalikan index dari voucher tersebut. jika tidak ada maka akan mengembalikan -1
+            
+        let potongan = 0                                                                    // INISIALISASI potongan dengan nilai default 0
+        if(promo !== -1){                                                                   // artinya voucher tersedia di dalam array
+            if(harga >= vouchers[promo].minHarga){                                          // memeriksa harga lebih dari atau sama dengan minimal harga dari voucher
+                potongan = harga * vouchers[promo].discon                                   // menentukan potongan dengan menghitung harga di kali dengan persentase discon
+                if(potongan >= vouchers[promo].maxPotongan){                                // jika potongan yang di dapat melebihi maximal potongan maka. maka nilai maximal potongan yang akan di pakai
+                    potongan = vouchers[promo].maxPotongan
+                }
+            }
+        }
+    
+        let biayaAntar = 5000                                                               // INISIALISASI biayaAntar dengan nilai default 5000
+        if(jarak > 2){                                                                      // jika jarak lebih dari 2 maka nilai biayaAntar akan di REASSIGNMENT dengan hasil perhitungan tersebut 
+            biayaAntar = (jarak - 2) * 3000 + biayaAntar
+        }
+    
+        let tambahanPajak = 0                                                               // INISIALISASI tambahanPajak dengan nilai default 0
+        if(pajak){                                                                          // jika pajak bernilai true maka tambahanPajak akan di REASSIGNMENT dengan hasil perhitungan tersebut
+            tambahanPajak = harga * 5/100
+        }
+    
+        const subTotal = (harga - potongan) + biayaAntar + tambahanPajak                   // menghitung total
+    
+        console.log(                                                                       // menampilkan output
+        `                                                                      
+            Harga : ${harga}
+            Potongan: ${potongan}
+            Biaya Antar : ${biayaAntar}
+            Pajak : ${tambahanPajak}
+            subTotal : ${subTotal}
+        `)
     }
-    return potongan                                                                     // jika discon lebih besar atau sama dengan potongan maka return nilai potongan yang di kirim di parameter
+    
+    fazzFood(75000, "FAZZFOOD50", 5, true)                                                  // memanggil fungsi fazzFood dengan mengirim parameter harga, voucher, jarak, dan pajak
 }
-
-const fazzFood = (harga, voucher, jarak, pajak) => {                                    // membuat fungsi yang menerima 4 parameter
-    if (jarak <= 0 || harga <= 0){                                                      // metode guarding untuk memeriksa jika inputan jarak atau harga kurang dari atau sama dengan 0 maka program di hentikan dan menampilkan pesan tersebut
-        return console.log("input tidak valid")
-    }
-
-    let biayaAntar = 5000                                                               // INISIALISASI biayaAntar dengan nilai default 5000
-    if(jarak > 2){                                                                      // jika jarak lebih dari 2 maka nilai biayaAntar akan di REASSIGNMENT dengan hasil perhitungan tersebut 
-        biayaAntar = (jarak - 2) * 3000 + biayaAntar
-    }
-
-    let tambahanPajak = 0                                                               // INISIALISASI tambahanPajak dengan nilai default 0
-    if(pajak){                                                                          // jika pajak bernilai true maka tambahanPajak akan di REASSIGNMENT dengan hasil perhitungan tersebut
-        tambahanPajak = harga * 5/100
-    }
-
-    let potongan = 0                                                                    // INISIALISASI potongan dengan nilai default 0
-    if(voucher === "FAZZFOOD50" && (harga >= 50000)){                                   // jika memenuhi kondisi maka potongan akan di REASSIGNMENT dengan pemanggilan fungsi potonganHarga dengan mengirim parameter nilai default potongan, harga, dan persentase discon
-        potongan = potonganHarga(50000, harga, 50)
-    }else if(voucher === "DITRAKTIR60" && (harga >= 25000)){
-        potongan = potonganHarga(30000, harga, 60)
-    }
-
-    const subTotal = (harga - potongan) + biayaAntar + tambahanPajak                   // menghitung total
-
-    console.log(                                                                       // menampilkan output
-    `                                                                      
-        Harga : ${harga}
-        Potongan: ${potongan}
-        Biaya Antar : ${biayaAntar}
-        Pajak : ${tambahanPajak}
-        subTotal : ${subTotal}
-    `)
-}
-
-fazzFood(75000, "FAZZFOOD50", 5, true)                                                 // memanggil fungsi fazzFood dengan mengirim parameter harga, voucher, jarak, dan pajak
--- task4.js
+++ task4.js
@@ -1,13 +1,13 @@
 // dengan built-in-function :
 
 {
-    const divideAndSort = (numbers) => {                        // membuat fungsi yg menerima 1 parameter
+    const divideAndSort = (numbers) => {                        // membuat fungsi yg menerima 1 parameter berupa NUMBER
     let string = ""                                             // INISIALISASI variable string dengan STRING kosong
 
     numbers                                                     // mengeksekusi inputan numbers dengan metode chaining
     .toString()                                                 // mengubah numbers jadi string =>  "5956560159466056"
     .split("0")                                                 // mengubah string menjadi array of string-number yang di pisah-pisah berdasarkan angka 0 => [ '595656', '159466', '56' ]
-    .map(arr => {                                               // tiap element dalam array di looping
+    .map(arr => {                                               // tiap element dalam array di looping => '595656' -> '159466' ->  '56'
         const array = Array.from(arr)                           // membuat array baru dari string. sehingga string-number menjadi di pisah-pisah tiap angka  => [ '5', '9', '5', '6', '5', '6' ] -> [ '1', '5', '9', '4', '6', '6' ]  ->  [ '5', '6' ]
                            .sort()                              // mengurutkan angka  => [ '5', '5', '5', '6', '6', '9' ] -> [ '1', '4', '5', '6', '6', '9' ] -> [ '5', '6' ] 
                            .join("")                            // membuat string baru dari array yang di konkatenasi => "555669" -> "145669" -> "56"
@@ -25,44 +25,63 @@
 
 
 
-// dengan Algoritma : belum selesai 
 
-// const sort = (arraySlice) => {
-//     for(let i = 0; i < arraySlice.length; i++){
-//         for(let j = 0; j < arraySlice[i].length; j++){
-//             for(let k = j + 1; k < arraySlice[i].length; k++){
-//                     // console.log( arraySlice[i][j], arraySlice[i][k])
-//                 if(arraySlice[i][j] > arraySlice[i][k]){
-//                     const shift = arraySlice[i][j]
-//                     arraySlice[i][j] = arraySlice[i][k]
-//                     arraySlice[i][k] = shift
-//                 }
-//             }
-//         }
-//         console.log(arraySlice[i])
-//     }
-// }
 
-// sort([ '595656', '159466', '56' ])
 
-// {
-//     const divideAndSort = (numbers) => {
-//         let stringNumbers = numbers + ""
-//         let slice = ""
-//         let arraySlice = []
 
-//         for(let i = 0; i < stringNumbers.length; i++){
-//             if(stringNumbers[i] !== "0"){
-//                 slice += stringNumbers[i]
-//             }else{
-//                 arraySlice = [...arraySlice, slice]
-//                 slice = ""
-//             }
-//         }
 
-//         arraySlice = [...arraySlice, slice]
 
-//         const output = callback(arraySlice)
-//     }
-//     divideAndSort(5956560159466056)
-// }
+
+
+
+
+
+
+//dengan Algoritma : 
+
+{
+    const divideAndSort = (numbers) => {                                                    // membuat fungsi yg menerima 1 parameter berisi nilai NUMBER
+        let stringNumbers = numbers + ""                                                    // merubah NUMBER menjadi STRING dengan cara menambahkan NUMBER dengan STRING kosong => "5956560159466056"
+
+        let slice = ""                                                                      // INISIALISASI slice dengan STRING kosong
+        let arraySlice = []                                                                 // INISIALISASI arraySlice dengan ARRAY kosong
+
+        for(let i = 0; i < stringNumbers.length; i++){                                      // melooping stringNumbers untuk mengambil tiap angka yang bertipe string
+            if(stringNumbers[i] !== "0"){                                                   // jika bukan karakter "0" maka akan di simpan di slice secara KONKATENASI => "595656" -> "159466" -> "56"
+                slice += stringNumbers[i]
+            }else{                                                                          // jika menemukan karakter "0" maka nilai variable slice akan di simpan di arraySlice => ["595656"] -> ["595656", "159466"]
+                arraySlice = [...arraySlice, slice]
+                slice = ""
+            }
+        }
+        arraySlice = [...arraySlice, slice]                                                 // hasil akhir arraySlice => ["595656", "159466", "56"]
+
+        let sortedString = ""                                                              // INISIALISASI sortedNumbers dengan ARRAY kosong. yg nantinya berisi numbers STRING yg sudah berurut
+
+        for(let i = 0; i < arraySlice.length; i++){                                         // melooping arraySlice yang berisi potongan-potongan STRING numbers
+
+            let segmentNumber = []                                                          // DEKLARASI segmentNumber dengan ARRAY kosong yg nantinya berisi ARRAY of NUMBER
+            for(let j = 0; j < arraySlice[i].length; j++){                                  // mengambil tiap angka yang di kali dengan true agar berubah menjadi tipe NUMBER lalu di simpan di ARRAY segmentNumber => [ 5, 9, 5, 6, 5, 6 ] -> [ 1, 5, 9, 4, 6, 6 ] -> [ 5, 6 ]
+                segmentNumber = [...segmentNumber, arraySlice[i][j] * true]
+            }
+
+            for(let x = 0; x < segmentNumber.length; x++){                                  // mengurutkan angka dengan bubble sort => [ 5, 5, 5, 6, 6, 9 ] -> [ 1, 4, 5, 6, 6, 9 ] -> [ 5, 6 ]
+                for(let y = x + 1; y < segmentNumber.length; y++){
+                    if(segmentNumber[x] > segmentNumber[y]){
+                        const shift = segmentNumber[x]
+                        segmentNumber[x] = segmentNumber[y]
+                        segmentNumber[y] = shift
+                    }
+                }
+            }
+
+            for(let n = 0; n < segmentNumber.length; n++){                                  // melooping segmentNumber untuk mengambil tiap angka yang di tambah dengan STRING kosong agar tipe NUMBER berubah jadi STRING kemudian di simpan di sortedString secara KONKATENASI => "55566914566956"
+                sortedString += segmentNumber[n] + ""
+            }
+        }
+
+        console.log(sortedString * true)                                                    // mengkalikan sortedString dengan true untuk merubah STRING menjadi NUMBER lalu di tampilkan => 55566914566956
+    }
+
+    divideAndSort(5956560159466056)                                                         // memanggil fungsi dividAndSort dan mengirim parameter NUMBER
+}
